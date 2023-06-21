22b85e7836ada0f17d720a72c4447b1cac8d9695
배승철 <paedison@gmail.com>
2023-06-21 13:39:29 +0900

    Update Favicon

--- _config/settings/prod.py
+++ _config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *  # noqa
 
 DEBUG = False
-ALLOWED_HOSTS = ['3.38.223.114']
+ALLOWED_HOSTS = ['3.38.223.114', 'paedison.com']
 
 # ALLOWED_HOSTS = env('DJANGO_ALLOWED_HOSTS').split()
