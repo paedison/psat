248eedcbd64dc4cba33f6058f6b23d11604e2c2a
배승철 <paedison@gmail.com>
2023-06-22 13:30:20 +0900

    Update Admin for Log Models

--- log/admin.py
+++ log/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from log.models import AccountLog, RequestLog, ProblemLog, LikeLog, RateLog, AnswerLog
+
+
+class GeneralLogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'timestamp', 'user', 'session_key', 'log_url', 'log_content')
+    list_per_page = 20
+
+
+class ProblemLogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'timestamp', 'user', 'session_key', 'problem')
+    list_per_page = 20
+
+
+class LikeLogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'timestamp', 'user', 'problem', 'is_liked')
+    list_per_page = 20
+
+
+class RateLogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'timestamp', 'user', 'problem', 'difficulty_rated')
+    list_per_page = 20
+
+
+class AnswerLogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'timestamp', 'user', 'problem', 'submitted_answer', 'is_correct')
+    list_per_page = 20
+
+
+admin.site.register(AccountLog, GeneralLogAdmin)
+admin.site.register(RequestLog, GeneralLogAdmin)
+admin.site.register(ProblemLog, ProblemLogAdmin)
+admin.site.register(LikeLog, LikeLogAdmin)
+admin.site.register(RateLog, RateLogAdmin)
+admin.site.register(AnswerLog, AnswerLogAdmin)
--- psat/admin.py
+++ psat/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Exam, Problem, Evaluation
+from psat.models import Exam, Problem, Evaluation
 
 
 class ExamAdmin(admin.ModelAdmin):
