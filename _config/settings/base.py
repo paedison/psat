b2c4b43df877627bc610f1bb357a5781e34ef99e
배승철 <paedison@gmail.com>
2023-07-05 11:38:25 +0900

    Update Settings

--- _config/settings/base.py
+++ _config/settings/base.py
@@ -57,6 +57,7 @@ if env_path.exists():
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 # Application definition
 INSTALLED_APPS = [
