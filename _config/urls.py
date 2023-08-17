07786f94cb2e655233a948d7e9fd1d25a4c2cdac
배승철 <paedison@gmail.com>
2023-08-17 12:46:41 +0900

    Change base url

--- _config/urls.py
+++ _config/urls.py
@@ -19,10 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from common.views.base_views import ads
-from psat.views.list_views import index
+from psat.views.list_views import index, ProblemListMainView
 
 urlpatterns = [
-    path('', index),
+    path('', ProblemListMainView.as_view()),
+    # path('', index),
     path('ads.txt', ads),
     path('admin/', admin.site.urls, name='admin'),  # 관리자 페이지
     path('notice/', include('notice.urls')),  # 공지사항
