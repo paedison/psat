f0a995e5587ed8191662917356f6f7dc436dab28
배승철 <paedison@gmail.com>
2023-09-18 12:03:00 +0900

    Update common js, header template

--- static/js/common.js
+++ static/js/common.js
@@ -1,36 +1,8 @@
-export const urls = {
-    /* Category or Menu */
-    'dashboard': '/dashboard/',
-    'profile': '/account/profile/',
-    'problem': '/psat/',
-    'psat': '/psat/',
-    'list': '/psat/',
-    'detail': '/psat/detail/',
-    'like': '/psat/like/',
-    'rate': '/psat/rate/',
-    'answer': '/psat/answer/',
-    'schedule': '/schedule/',
-
-    /* Type */
-    'dashboardList': '/dashboard/',
-    'profileList': '/account/profile/',
-    'problemList': '/psat/',
-    'problemDetail': '/psat/detail/',
-    'likeList': '/psat/like/',
-    'likeDetail': '/psat/like/',
-    'rateList': '/psat/rate/',
-    'rateDetail': '/psat/rate/',
-    'answerList': '/psat/answer/',
-    'answerDetail': '/psat/answer/',
-    'scheduleList': '/schedule/',
-}
+const info = JSON.parse($('#info').text());
 const psat_menu = ['problem', 'like', 'rate', 'answer']
 const psat_type = ['problemList', 'likeList', 'rateList', 'answerList']
-
-export let info = JSON.parse($('#info').text());
-let menu = info['menu'] || $('#info').text();
-let type = info['type'] || $('#info').text();
-export let csrf_token = $("input[name='csrfmiddlewaretoken']").val();
+const menu = info['menu'];
+const type = info['type'];
 
 
 // Toggle the side navigation
@@ -39,7 +11,7 @@ $("#sidebarToggle").on('click', function() {
 });
 
 
-/* Expand & activate menu */
+// Expand & activate menu
 function expandMenu(target) {
     const navContent = $(target).closest('ul');
     const navLink = navContent.prev('a');
@@ -51,28 +23,8 @@ function expandMenu(target) {
     bulletPoint.removeClass('fa-regular').addClass('fa-solid');
 }
 
-$(document).on('click', '#sidebar-nav .nav-link', function() {
-    let bsTarget = $(this).data('bsTarget');
-    $('.nav-link').addClass('collapsed').attr('aria-expanded', 'false');
-    $(this).removeClass('collapsed').attr('aria-expanded', 'true');
-    $('#psat-nav').removeClass('show');
-    $('.aside-nav-icon').removeClass('active').find('i').removeClass('fa-solid').addClass('fa-regular');
-    if (bsTarget !== '#psat-nav') {
-        $("body").toggleClass("toggle-sidebar");
-    }
-});
-
-$(document).on('click', '.aside-nav-icon', function() {
-    $('.nav-link').addClass('collapsed').attr('aria-expanded', 'false');
-    $(this).closest('ul').prev('a').removeClass('collapsed').attr('aria-expanded', 'true');
-
-    $(this).closest('li').children().removeClass('active');
-    $(this).closest('li').children().find('i').removeClass('fa-solid').addClass('fa-regular');
-
-    $(this).addClass('active').find('i').removeClass('fa-regular').addClass('fa-solid');
-    $("body").toggleClass("toggle-sidebar");
-});
-
+// When clicked the main menu [Notice, Dashboard, PSAT, Schedule, Score]
+// menu = notice | dashboard | problem | schedule | score
 $(document).ready(function() {
     const targetLink = `#${menu}List`;
     if (psat_menu.includes(menu)) {
@@ -82,7 +34,8 @@ $(document).ready(function() {
     }
 });
 
-
+// When clicked the PSAT sub-menu [Problem, Like, Rate, Answer]
+// type = problem | like | rate | answer
 $(document).ready(function() {
     const targetLink = `#${type}`;
     if (psat_type.includes(type)) {
@@ -90,6 +43,42 @@ $(document).ready(function() {
     } else {
         $(targetLink).removeClass('collapsed');
     }
+});
+
+
+// Initialize the side menu bar
+function initialMenu(target) {
+    $('.nav-link').addClass('collapsed').attr('aria-expanded', 'false');
+    $(target).removeClass('collapsed').attr('aria-expanded', 'true');
+    $('#psat-nav').removeClass('show');
+    $('.aside-nav-icon').removeClass('active').find('i').removeClass('fa-solid').addClass('fa-regular');
+
+}
+
+// When clicked the logo
+$(document).on('click', '.logo', function() {
+    initialMenu($(this).data('target'));
+    $('body').removeClass('toggle-sidebar');
+});
+
+// When clicked the main menu [Notice, Dashboard, PSAT, Schedule, Score]
+$(document).on('click', '#sidebar-nav .nav-link', function() {
+    initialMenu(this);
+    if ($(this).data('bsTarget') !== '#psat-nav') {
+        $('body').toggleClass('toggle-sidebar');
+    }
+});
+
+// When clicked the PSAT sub-menu [Problem, Like, Rate, Answer]
+$(document).on('click', '.aside-nav-icon', function() {
+    $('.nav-link').addClass('collapsed').attr('aria-expanded', 'false');
+    $(this).closest('ul').prev('a').removeClass('collapsed').attr('aria-expanded', 'true');
+
+    $(this).closest('li').children().removeClass('active');
+    $(this).closest('li').children().find('i').removeClass('fa-solid').addClass('fa-regular');
+
+    $(this).addClass('active').find('i').removeClass('fa-regular').addClass('fa-solid');
+    $('body').toggleClass('toggle-sidebar');
 });
 
 
@@ -117,3 +106,30 @@ $(document).ready(function() {
 //         prevScrollPos = currentScrollPos;
 //     });
 // });
+
+// export const urls = {
+//     // Category or Menu
+//     'dashboard': '/dashboard/',
+//     'profile': '/account/profile/',
+//     'problem': '/psat/',
+//     'psat': '/psat/',
+//     'list': '/psat/',
+//     'detail': '/psat/detail/',
+//     'like': '/psat/like/',
+//     'rate': '/psat/rate/',
+//     'answer': '/psat/answer/',
+//     'schedule': '/schedule/',
+//
+//     // Type
+//     'dashboardList': '/dashboard/',
+//     'profileList': '/account/profile/',
+//     'problemList': '/psat/',
+//     'problemDetail': '/psat/detail/',
+//     'likeList': '/psat/like/',
+//     'likeDetail': '/psat/like/',
+//     'rateList': '/psat/rate/',
+//     'rateDetail': '/psat/rate/',
+//     'answerList': '/psat/answer/',
+//     'answerDetail': '/psat/answer/',
+//     'scheduleList': '/schedule/',
+// }
